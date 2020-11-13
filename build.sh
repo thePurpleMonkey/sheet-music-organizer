a3c49cc8d6ee02a04d2a46edf7fd3cc6c46c0afb
Michael Humphrey <mhumphree@gmail.com>
2020-11-13 08:25:24 -0800

    Added linux build script

---
+++ build.sh
@@ -0,0 +1,15 @@
+export SESSION_KEY=$(cat session_key.txt)
+export SENDGRID_API_KEY=$(cat sendgrid_api_key.txt)
+export HOST=smo.michaelhumphrey.dev
+export CERT_FILE=/etc/letsencrypt/live/michaelhumphrey.dev/fullchain.pem
+export KEY_FILE=/etc/letsencrypt/live/michaelhumphrey.dev/privkey.pem
+export PORT=8000
+export DB_USERNAME=smo_user
+export DB_PASSWORD=$(cat db_password.txt)
+go build -ldflags="-linkmode=internal -extld=none"
+if [ $? -ne 0 ]; then
+	echo Build failed.
+else
+	echo Starting server...
+	sudo --preserve-env ./sheet-music-organizer & &> ~/sheet-music-organizer.log
+fi
