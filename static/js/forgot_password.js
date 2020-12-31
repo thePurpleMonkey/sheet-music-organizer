ce16881ef1498927de3718352df842c651b84c6d
Michael Humphrey <mhumphree@gmail.com>
2020-12-31 13:29:30 -0800

    Changed password reset wording

---
+++ static/js/forgot_password.js
@@ -0,0 +1,36 @@
+"use strict";
+
+import { add_alert, alert_ajax_failure } from "/js/utilities.js";
+
+// Show navbar options
+$("#navbar_logout").hide();
+$("#navbar_account").hide();
+$("#navbar_login").show();
+$("#navbar_register").show();
+
+$("#request_reset").click(function() {
+	$("#wait").modal("show");
+});
+$('#wait').on('shown.bs.modal', function (e) {
+	$.post("/user/password/forgot", JSON.stringify({email: $("#email").val()}))
+		.done(function( data ) {
+			$("form").hide(500);
+			$("#email_sent").show(500);
+		})
+		.fail(function(data) {
+			console.log(data);
+			alert_ajax_failure(data);
+			// $("#reason").text(data.responseJSON.error);
+			// $(".alert").show()
+		})
+		.always(function() {
+			$("#wait").modal("hide")
+		});
+});
+
+$('#email').keypress(function (e) {
+	if (e.which === 13) {
+		$('#request_reset').click();
+		return false;
+	}
+});
