0ff51e67dd60e8b25784a3070401b0ec2de06ea3
Michael Humphrey <mhumphree@gmail.com>
2021-07-19 10:17:07 -0700

    Cleaned up linux build script

--- build.sh
+++ build.sh
@@ -3,7 +3,4 @@
 go build -ldflags="-linkmode=internal -extld=none"
 if [ $? -ne 0 ]; then
 	echo Build failed.
-# else
-# 	echo Starting server...
-# 	sudo --preserve-env ./sheet-music-organizer &>> /home/michael/sheet-music-organizer.log &
 fi
